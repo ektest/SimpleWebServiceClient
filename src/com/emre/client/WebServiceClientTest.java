229573ed462922e70f9d0c459c75066f0a4d0a4b
Emre Koca <ekoca1@gmail.com>
2014-05-31 17:56:32 -0700

    WebService client v2

--- src/com/emre/client/WebServiceClientTest.java
+++ src/com/emre/client/WebServiceClientTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.emre.staffmanagement.Employee;
 import com.emre.staffmanagement.EmployeeManagementImplementationService;
 import com.emre.staffmanagement.EmployeeManagementWebService;
+import com.emre.staffmanagement.SystemUnavaliableException;
+import com.emre.staffmanagement.SystemUnavaliableException_Exception;
 
 public class WebServiceClientTest {
 
@@ -13,11 +15,25 @@ public class WebServiceClientTest {
 				new EmployeeManagementImplementationService().getEmployeeManagementWebServicePort();
 
 		List<Employee> employees = service.getAllEmployees();
-		
+
 		for (Employee next : employees)
 		{
 			System.out.println(next);
+			System.out.println(next.getFirstName() + " " + next.getSurname());
+		}
+
+		Employee emp = new Employee();
+		emp.setFirstName("Bill");
+		emp.setSurname("Bill");
+		emp.setJobRole("Actor");
+		emp.setSalary(1899);
+		
+		try{
+			service.registerEmployee(emp);
+			
+		}catch(SystemUnavaliableException_Exception e)
+		{
+			System.out.println("System is down or unavaliable");
 		}
 	}
-
 }
